24f4b85a87ad19ca8a813a1c452eaaeee8c80083
Santiago Rodriguez <santiago.rodrigue@nbcuni.com>
2023-12-18 17:03:41 -0400

    do not include a coloscheme

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,23 +39,6 @@ return require('packer').startup(function(use)
     end
   }
 
-  -- Color themes
-  use({
-    "themercorp/themer.lua",
-    config = function()
-      require("themer").setup({
-        colorscheme = "everforest",
-        styles = {
-          ["function"] = { style = 'italic' },
-          functionbuiltin = { style = 'italic' },
-          variable = { style = 'italic' },
-          variableBuiltIn = { style = 'italic' },
-          parameter  = { style = 'italic' },
-        },
-      })
-    end
-  })
-
   -- Status line
   use {
     'nvim-lualine/lualine.nvim',
