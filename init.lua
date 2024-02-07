10bc2cf31da06bc0abe86a07e928ecca2551ec57
Santiago Rodriguez <santiago.rodrigue@nbcuni.com>
2024-02-07 17:35:50 -0400

    keep default theme

--- init.lua
+++ init.lua
@@ -1,2 +1,7 @@
-require('base')
-require('plugins')
+vim.opt.sw = 2
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+vim.opt.wrap = false
+vim.opt.number = true
+vim.opt.relativenumber = true
--- lua/base.lua
+++
@@ -1,9 +0,0 @@
-vim.opt.sw = 2
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.g.mapleader = ' '
-vim.opt.wrap = false
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.cursorline = true
