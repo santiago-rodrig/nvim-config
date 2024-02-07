217f3fc8087797704c9e8d19c027a43e3598d5c7
Santiago Rodriguez <santiago.rodrigue@nbcuni.com>
2024-02-07 18:56:13 -0400

    add treesitter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,5 +5,14 @@ return require('packer').startup(function(use)
   -- Kanagawa colorscheme
   use 'rebelot/kanagawa.nvim'
 
+  -- Language server configurations
   use 'neovim/nvim-lspconfig'
+
+  -- Treesitter, better syntax highlighting and indetation
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      require('nvim-treesitter.install').update({ with_sync = true })
+    end
+  }
 end)
